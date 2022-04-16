a6f087490ecf34f8d47fe6c9744a6acb69376656
AnadiSharma007 <anadis764@gmail.com>
2022-04-16 15:53:50 +0530

    added get/create post controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,26 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPost = (req, res) => {
-    res.send("This Works");
+export const getPost = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(400).json({ message: error.message});
+    }
 }
 
 export const createPost = (req, res) => {
-    res.send("Post Creation");
+    const post = req.body;
+    
+    const newPost = new PostMessage(post);
+    try {
+        newPost.save();
+
+        res.status(201).json(newPost);
+        
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
 
