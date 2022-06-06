e91326034feae75eb7ddd2dcf3cc3d23c46584c6
AnadiSharma007 <anadis764@gmail.com>
2022-06-06 10:51:08 +0530

    backend code

--- controllers/user.js
+++ controllers/user.js
@@ -9,13 +9,13 @@ export const signin = async (req, res) => {
     try {
         const existingUser = await User.findOne({ email });
 
-        if(!existingUser) return res.status(404).json({ message: "User doen't exist." });
+        if(!existingUser) return res.status(404).json({ message: "User doesn't exist." });
 
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
 
         if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid Credentials." });
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h"});
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', {});
 
         res.status(200).json({ result: existingUser, token });
 
@@ -32,13 +32,13 @@ export const signup = async (req, res) => {
 
         if(existingUser) return res.status(400).json({ message: "User already exist."});
 
-        if(password !== confirmPassword) return res.status(400).json({ message: "Password don't match" });
+        if(password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match." });
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
 
-        const token = jwt.sign({ email: result.email, id: result._id}, 'test', { expiresIn: "1h"});
+        const token = jwt.sign({ email: result.email, id: result._id}, 'test', {});
 
         res.status(200).json({ result, token });
     } catch (error) {
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 
@@ -8,10 +8,12 @@ const router = express.Router();
 
 // http://localhost:5000/posts
 
+router.get('/:id', getPost);
+router.get('/search', getPostsBySearch);
 router.get("/", getPosts);
 router.post("/", auth, createPost);
-router.patch("/:id",auth, updatePost);
-router.delete("/:id",auth, deletePost);
-router.patch("/:id/likePost",auth, likePost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
