2a5426a71ad33b0ae3b3a7d4beb6ff6c26856b60
AnadiSharma007 <anadis764@gmail.com>
2022-06-12 17:27:17 +0530

    fixing bugs

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ import  express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv'
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 
 const app = express();
-dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
@@ -20,7 +19,8 @@ app.use("/user", userRoutes);
 app.get('/', (req, res) => {
     res.send("APP IS RUNNING.");
 })
-CONNECTION_URL = `mongodb+srv://anadi:12345@cluster0.w1a9j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+const CONNECTION_URL = `mongodb+srv://anadi:12345@cluster0.w1a9j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
