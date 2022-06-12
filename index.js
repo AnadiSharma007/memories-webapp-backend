54c0c012383ab60afa84c3f4c0ae721919ff412c
AnadiSharma007 <anadis764@gmail.com>
2022-06-12 17:19:07 +0530

    updated index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.use("/user", userRoutes);
 app.get('/', (req, res) => {
     res.send("APP IS RUNNING.");
 })
-
+CONNECTION_URL = `mongodb+srv://anadi:12345@cluster0.w1a9j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
